dc148ced87f571b50480456dc06ec34d2020360f
Techhackontime999 <amitkumarkh01012006@gmail.com>
2024-10-04 09:43:48 +0530

    setup

---
+++ DR_SERVE/HOSPITAL_APP/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def Home(request):
+    return HttpResponse("hello hospital home")
+    # return render(request,'HOSPITAL_APP/Home.html')
+    
