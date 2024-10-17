283bcfcd80b91f90779267164a92c829dc7edff9
Techhackontime999 <amitkumarkh01012006@gmail.com>
2024-10-17 20:19:51 +0530

    adding searching DRSERVE |Contactus

--- DR_SERVE/DR_SERVE/urls.py
+++ DR_SERVE/DR_SERVE/urls.py
@@ -19,7 +19,6 @@ from django.urls import path,include
 from .import views
 from django.conf import settings
 from django.conf.urls.static import static
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Home,name='Home'),
